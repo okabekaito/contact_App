7938923e08397564eb68b4cd24e9ff067739185f
okabekaito <kaito19991204@gmail.com>
2021-11-08 17:34:47 +0900

    nickname

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,8 @@ class PostsController < ApplicationController
   # GET /posts or /posts.json
   def index
     @posts = Post.page(params[:page]).per(10)
+    @users = User.all
+    
   end
 
   # GET /posts/1 or /posts/1.json
